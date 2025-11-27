4af0303944a1dd3d646e7b40a36996abc54e08f2
kirubhel <kirub.hel@gmail.com>
2025-11-27 11:39:39 +0300

    Add Vercel configuration for automatic deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    // Enable image optimization on Vercel
+    unoptimized: false,
+  },
 }
 
 module.exports = nextConfig
